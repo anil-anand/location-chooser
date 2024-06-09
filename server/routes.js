068566c5a94df24bd0787a961d862926b52371b9
Anil Anand <anilanandchess@gmail.com>
2024-06-09 18:54:58 +0530

    Added API endpoints for loading all captures, showing a capture and creating a capture

---
+++ server/routes.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const router = express.Router();
+const MapCapture = require("./models/MapCapture");
+
+router.post("/save", async (request, response) => {
+  try {
+    const { latitude, longitude, zoom, image, width, height } = request.body;
+    const newCapture = new MapCapture({
+      latitude,
+      longitude,
+      zoom,
+      image,
+      width,
+      height,
+    });
+    await newCapture.save();
+    response.status(201).send("Saved");
+  } catch (error) {
+    response.status(500).send(error.message);
+  }
+});
+
+router.get("/captures", async (_, response) => {
+  try {
+    const captures = await MapCapture.find().sort({ createdAt: -1 });
+    response.json(captures);
+  } catch (error) {
+    response.status(500).send(error.message);
+  }
+});
+
+router.get("/captures/:id", async (request, response) => {
+  try {
+    const capture = await MapCapture.findOne({ _id: request.params.id });
+    response.json(capture);
+  } catch (error) {
+    response.status(404).send(`Capture with ID ${request.params.id} not found`);
+  }
+});
+
+module.exports = router;
--- server/server.js
+++ server/server.js
@@ -3,6 +3,8 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 
+const routes = require("./routes");
+
 require("dotenv").config();
 
 const app = express();
@@ -18,6 +20,7 @@ mongoose
 
 app.use(cors());
 app.use(bodyParser.json({ limit: "10mb" }));
+app.use("/", routes);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
