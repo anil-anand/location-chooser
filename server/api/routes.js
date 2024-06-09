0c29e0ee8ab42d7a42762a769f6d4ab584032922
Anil Anand <anilanandchess@gmail.com>
2024-06-10 00:41:29 +0530

    Added API for getting top regions

--- server/api/routes.js
+++ server/api/routes.js
@@ -2,7 +2,11 @@ const express = require("express");
 const axios = require("axios");
 const NodeCache = require("node-cache");
 
-const { CACHE_TTL, CAPTURES_CACHE_KEY } = require("../constants");
+const {
+  CACHE_TTL,
+  CAPTURES_CACHE_KEY,
+  TOP_REGIONS_CACHE_KEY,
+} = require("../constants");
 const MapCapture = require("../models/MapCapture");
 
 const router = express.Router();
@@ -20,7 +24,7 @@ router.post("/save", async (request, response) => {
       height,
     });
     await newCapture.save();
-    myCache.del(CAPTURES_CACHE_KEY);
+    myCache.del([CAPTURES_CACHE_KEY, TOP_REGIONS_CACHE_KEY]);
     response.status(201).json({ message: "Saved", id: newCapture._id });
   } catch (error) {
     response.status(500).send(error.message);
@@ -55,6 +59,51 @@ router.get("/captures/:id", async (request, response) => {
     }
   } catch (error) {
     response.status(404).send(`Capture with ID ${request.params.id} not found`);
+  }
+});
+
+router.get("/top-regions", async (_, response) => {
+  try {
+    if (myCache.has(TOP_REGIONS_CACHE_KEY)) {
+      response.json(myCache.get(TOP_REGIONS_CACHE_KEY));
+      return;
+    }
+
+    const captures = await MapCapture.aggregate([
+      {
+        $group: {
+          _id: {
+            latitude: "$latitude",
+            longitude: "$longitude",
+            zoom: "$zoom",
+          },
+          count: { $sum: 1 },
+        },
+      },
+      { $sort: { count: -1 } },
+      { $limit: 3 },
+    ]);
+
+    const geoCodedRegions = await Promise.all(
+      captures.map(async (region) => {
+        const { latitude, longitude } = region._id;
+
+        const response = await axios.get(
+          `https://maps.googleapis.com/maps/api/geocode/json?latlng=${latitude},${longitude}&key=${process.env.GOOGLE_MAPS_API_KEY}&language=en`
+        );
+        const address =
+          response.data.results[0]?.formatted_address || "Unknown location";
+        return {
+          ...region,
+          name: address,
+        };
+      })
+    );
+
+    myCache.set(TOP_REGIONS_CACHE_KEY, geoCodedRegions);
+    response.json(geoCodedRegions);
+  } catch (error) {
+    response.status(500).send(error.message);
   }
 });
 
--- server/constants.js
+++ server/constants.js
@@ -1,4 +1,5 @@
 const CAPTURES_CACHE_KEY = "all-captures";
+const TOP_REGIONS_CACHE_KEY = "top-regions";
 const CACHE_TTL = 60 * 60 * 24; // 1 day
 
-module.exports = { CACHE_TTL, CAPTURES_CACHE_KEY };
+module.exports = { CACHE_TTL, CAPTURES_CACHE_KEY, TOP_REGIONS_CACHE_KEY };
