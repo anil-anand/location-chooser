8031ea896f55efafe7743b25ffc885a2d37fb91e
Anil Anand <54279092+anil-anand@users.noreply.github.com>
2024-06-09 18:57:06 +0530

    Created backend basics (#2)
    * Added `MapCapture` model
    
    * Removed server stuff from root directory
    
    * Modified `.gitignore`
    
    * Added `server.js`
    
    * Added API endpoints for loading all captures, showing a capture and creating a capture
    
    * package.json
    
    * .env file

--- server/server.js
+++ server/server.js
@@ -1,17 +1,15 @@
-// server.js
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+const bodyParser = require("body-parser");
+
+const routes = require("./routes");
+
 require("dotenv").config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8000;
 
-// Middleware
-app.use(cors());
-app.use(express.json());
-
-// MongoDB connection
 mongoose
   .connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
@@ -20,10 +18,9 @@ mongoose
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.error(err));
 
-// Routes
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+app.use(cors());
+app.use(bodyParser.json({ limit: "10mb" }));
+app.use("/", routes);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
