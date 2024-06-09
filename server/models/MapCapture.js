e108a8297fd8b096397d913f6a0b1c9701b666d6
Anil Anand <anilanandchess@gmail.com>
2024-06-09 18:51:02 +0530

    Added `MapCapture` model

---
+++ server/models/MapCapture.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+const mapCaptureSchema = new mongoose.Schema({
+  latitude: { type: Number, required: true },
+  longitude: { type: Number, required: true },
+  zoom: { type: Number, required: true },
+  width: { type: Number, required: true, default: 600 },
+  height: { type: Number, required: true, default: 400 },
+});
+
+const MapCapture = mongoose.model("MapCapture", mapCaptureSchema);
+
+module.exports = MapCapture;
