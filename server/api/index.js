cce1498f32f8cdfef30d623dea548801430c35df
Anil Anand <anilanandchess@gmail.com>
2024-06-09 23:10:02 +0530

    Modified env variable's name

--- server/api/index.js
+++ server/api/index.js
@@ -11,7 +11,7 @@ const app = express();
 const PORT = process.env.PORT || 8000;
 
 mongoose
-  .connect(process.env.MONGO_URI, {
+  .connect(process.env.MONGODB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
