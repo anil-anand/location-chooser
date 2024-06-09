ffe6f6b54b96283b07eb959a946bf5c9b2f35f65
Anil Anand <anilanandchess@gmail.com>
2024-06-10 01:50:22 +0530

    Added an enhancement

--- server/models/MapCapture.js
+++ server/models/MapCapture.js
@@ -1,13 +1,16 @@
 const mongoose = require("mongoose");
 
-const mapCaptureSchema = new mongoose.Schema({
-  title: { type: String, required: true, default: "Map Capture" },
-  latitude: { type: Number, required: true },
-  longitude: { type: Number, required: true },
-  zoom: { type: Number, required: true },
-  width: { type: Number, required: true, default: 600 },
-  height: { type: Number, required: true, default: 400 },
-});
+const mapCaptureSchema = new mongoose.Schema(
+  {
+    title: { type: String, required: true, default: "Map Capture" },
+    latitude: { type: Number, required: true },
+    longitude: { type: Number, required: true },
+    zoom: { type: Number, required: true },
+    width: { type: Number, required: true, default: 600 },
+    height: { type: Number, required: true, default: 400 },
+  },
+  { timestamps: true }
+);
 
 const MapCapture = mongoose.model("MapCapture", mapCaptureSchema);
 
